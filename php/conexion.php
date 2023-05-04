72da15a8819a740020161e8ff1cbb4dd15b275cd
Jeremy Hernández Balderas <jeremy.hdez9@gmail.com>
2023-05-03 22:11:43 -0600

    ..

--- php/conexion.php
+++ php/conexion.php
@@ -5,7 +5,7 @@ $servidor="localhost";
 $basededatos="db_hospital";
 //$GLOBAL ["conexion"];
 $conexion = mysqli_connect($servidor,$usuario,$contrasena) or 
-die ("No se ha podido conectar al servidor de Base de Datos");
+die ("No se ha podido conectar al servidor");
 
 $db = mysqli_select_db($conexion,$basededatos) or 
 die("No se ha podido conectar al servidor de Base de Datos");
